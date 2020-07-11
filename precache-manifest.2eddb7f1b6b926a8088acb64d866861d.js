5f2e782d3ecab7a17c96685ac90b1b6fbcaa3a56
calvang <calvin.huang5@gmail.com>
2020-07-11 03:48:39 -0400

    Updates

---
+++ precache-manifest.2eddb7f1b6b926a8088acb64d866861d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3feaad2415d600a93bacd45d7c9b0dc2",
+    "url": "/calvang-main/index.html"
+  },
+  {
+    "revision": "a1734cdb39cfe43e6064",
+    "url": "/calvang-main/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46b5906eaaaccb9b5b4a",
+    "url": "/calvang-main/static/js/2.119a99ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calvang-main/static/js/2.119a99ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1734cdb39cfe43e6064",
+    "url": "/calvang-main/static/js/main.0bd0d329.chunk.js"
+  },
+  {
+    "revision": "b600366bdf2cff939784",
+    "url": "/calvang-main/static/js/runtime-main.8b1ea684.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calvang-main/static/media/logo.5d5d9eef.svg"
+  }
+]);
